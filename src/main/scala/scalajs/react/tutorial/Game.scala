a3ce3ff1112e29e3d443061d4e27b0b059488a6f
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2020-04-25 17:45:08 +0900

    Showing the Past Moves / Picking a Key / Implementing Time Travel

--- src/main/scala/scalajs/react/tutorial/Game.scala
+++ src/main/scala/scalajs/react/tutorial/Game.scala
@@ -47,30 +47,47 @@ object AppCSS extends js.Object
 
 @react class Game extends Component {
   type Props = Unit
-  case class State(history: Vector[Vector[Option[String]]], xIsNext: Boolean)
+  case class State(history: Vector[Vector[Option[String]]], stepNumber: Int, xIsNext: Boolean)
 
-  def initialState: State = State(Vector(Vector.fill(9)(None)), true)
+  def initialState: State = State(Vector(Vector.fill(9)(None)), 0, true)
 
   private val css = AppCSS
 
   def handleClick(i: Int): Unit = {
-    val current = state.history.last
+    val history = state.history.slice(0, state.stepNumber + 1)
+    val current = history.last
     if (Utils.calculateWinner(current).isDefined || current(i).isDefined) ()
     else
       setState(
         state.copy(
-          history = state.history :+ current.updated(i, if (state.xIsNext) Some("X") else Some("O")),
+          history = history :+ current.updated(i, if (state.xIsNext) Some("X") else Some("O")),
+          stepNumber = history.length,
           xIsNext = !state.xIsNext
         )
       )
   }
 
+  def jumpTo(step: Int): Unit = setState { s =>
+    s.copy(
+      stepNumber = step,
+      xIsNext = (step % 2) == 0
+    )
+  }
+
   def render(): ReactElement = {
-    val current = state.history.last
+    val current = state.history(state.stepNumber)
     val winner  = Utils.calculateWinner(current)
     val status = winner match {
       case Some(winner) => s"Winner: $winner"
       case None         => s"Next player: ${if (state.xIsNext) "X" else "O"}"
+    }
+
+    val moves = state.history.zipWithIndex.map {
+      case (step, move) =>
+        val desc = if (move == 0) "Go to game start" else s"Go to move #$move"
+        li(key := move.toString)(
+          button(onClick := (_ => jumpTo(move)))(desc)
+        )
     }
 
     div(className := "game")(
@@ -79,7 +96,7 @@ object AppCSS extends js.Object
       ),
       div(className := "game-info")(
         div(status),
-        div() // TODO
+        div(moves)
       )
     )
   }
